83606099e1e01bd7251471dfc96a4cab8475f311
IV-AML <aml@infoventure.dk>
2025-09-18 16:35:09 +0200

    Update AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { App } from './App';
-import { ManagementOverview } from './pages/ManagementOverview';
+// AppRouter.tsx
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { App } from './App'
+import { ManagementOverview } from './pages/ManagementOverview'
+
 export const AppRouter: React.FC = () => {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter basename="/CBC_PlanningDashBoard">
       <Routes>
         <Route path="/" element={<ManagementOverview />} />
         <Route path="/citizens" element={<App />} />
+        {/* catch-all to avoid blanks on unknown paths */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>;
-};
+    </BrowserRouter>
+  )
+}
