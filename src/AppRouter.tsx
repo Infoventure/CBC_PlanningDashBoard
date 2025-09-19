efbc8c6d48bf31bc314907d71117e97057688460
IV-AML <aml@infoventure.dk>
2025-09-19 11:46:20 +0200

    [Magic Patterns] Updated from 'New Design'

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,18 +1,15 @@
 // AppRouter.tsx
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { App } from './App'
-import { ManagementOverview } from './pages/ManagementOverview'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { App } from './App';
+import { ManagementOverview } from './pages/ManagementOverview';
 export const AppRouter: React.FC = () => {
-  return (
-    <BrowserRouter basename="/CBC_PlanningDashBoard">
+  return <BrowserRouter basename="/CBC_PlanningDashBoard">
       <Routes>
         <Route path="/" element={<ManagementOverview />} />
         <Route path="/citizens" element={<App />} />
         {/* catch-all to avoid blanks on unknown paths */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
-  )
-}
+    </BrowserRouter>;
+};
