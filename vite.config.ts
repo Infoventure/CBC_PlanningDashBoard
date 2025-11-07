2129774fd8bc1cd82469492053383b30f5a35f22
Markus Bjerre Nielsen <mbn@casebycase.dk>
2025-11-07 10:54:01 +0100

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isDev = 'dev';
+const base = '/CBC_PlanningDashBoard/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isDev
-    ? '/CBC_PlanningDashBoard-dev/'
-    : '/CBC_PlanningDashBoard/',
+  base: base,
+    plugins: [react()],
 })
